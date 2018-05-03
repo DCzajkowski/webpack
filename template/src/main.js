89ebbba04b1eb1c53a8f5b55c2dd8ef8e99ba229
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-05-04 01:26:26 +0200

    Spacing

--- template/src/main.js
+++ template/src/main.js
@@ -1,26 +1,24 @@
-{{#if_eq build "standalone"}}
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-{{/if_eq}}
 import Vue from 'vue'
-import App from './App'
+Vue.config.productionTip = false
+Vue.config.devtools = true
+
 {{#router}}
 import router from './router'
 {{/router}}
 
-Vue.config.productionTip = false
+import App from './App'
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  {{#router}}
-  router,
-  {{/router}}
-  {{#if_eq build "runtime"}}
-  render: h => h(App)
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
-  components: { App },
-  template: '<App/>'
-  {{/if_eq}}
+    el: '#app',
+    {{#router}}
+    router,
+    {{/router}}
+    {{#if_eq build "runtime"}}
+    render: h => h(App)
+    {{/if_eq}}
+    {{#if_eq build "standalone"}}
+    components: { App },
+    template: '<App/>'
+    {{/if_eq}}
 })
